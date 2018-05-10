964ff308443eac173dfe2e25da7b4539e4dbc1ca
kenny <kennydaltonc@yahoo.com>
2018-05-10 01:43:31 -0400

    proyectos correcgidos estado de proyecto,fecha registro proyecto

--- app/Proyecto.php
+++ app/Proyecto.php
@@ -9,7 +9,7 @@ class Proyecto extends Model
     //le damos el nombre a la tabla
     protected $table = 'proyecto';
     protected $primaryKey = 'idProyecto';
-    protected $fillable = ['titulo','objetivos','descripcion', 'fecha', 'fechaFin', 'periodo', 'sesionDeConsejo', 'idModalidad'];
+    protected $fillable = ['titulo','objetivos','descripcion', 'fechaIni', 'fechaFin', 'periodo', 'sesionDeConsejo', 'idModalidad','estadoProyecto','fechaRegistroProy'];
     //ya no guarda el created_at, updated_at 
     public $timestamps = false;
 
