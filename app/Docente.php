7a5272a8daf77a17a8d1c8b4d31dcd0a1d63f0e0
kenny <kennydaltonc@yahoo.com>
2018-05-23 23:39:57 -0400

    agregar docentes y asignar proyectos

--- app/Docente.php
+++ app/Docente.php
@@ -9,7 +9,7 @@ class Docente extends Model
     //le damos el nombre a la tabla
     protected $table = 'docente';
     protected $primaryKey = 'idDoc';
-    protected $fillable = ['ciDoc','nombreDoc', 'apePaternoDoc', 'apeMaternoDoc', 'emailDoc', 'telefonoDoc', 'tituloDoc', 'cargaHoraria', 'codigoDoc'];
+    protected $fillable = ['ciDoc','nombreDoc', 'apePaternoDoc', 'apeMaternoDoc', 'emailDoc', 'telefonoDoc', 'tituloDoc', 'cargaHoraria'];
     //ya no guarda el created_at, updated_at 
     public $timestamps = false;
 }
