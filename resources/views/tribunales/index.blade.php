7449e8df185d5365ad6ca12b8bc0ddd03844aba6
LuisEduardo <ketchsoul@gmail.com>
2018-05-28 14:04:11 -0400

    feeeer!

--- resources/views/tribunales/index.blade.php
+++ resources/views/tribunales/index.blade.php
@@ -1,54 +1,29 @@
 @extends('layouts.app')
 @section('content')
-{{ csrf_field() }}
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <h1 class="text-center">Tribunales</h1>
 <div class="form-group row">
-    <label for="buscador" class="col-sm-2 col-form-label col-form-label-lg">Buscador :</label>
+    <label for="buscador" class="col-sm-2 col-form-label col-form-label-lg">Buscar :</label>
     <div class="col-sm-10">
-        <input class="form-control form-control-lg" id="Search1" type="text" placeholder="Tribunal..">	
+        <input class="form-control form-control-lg" id="buscador" type="text" placeholder="Tribunal..">	
     </div>
 </div>
-<div >
-    <table class="table table-bordered table-striped tablaScroll3 ">
+<div>
+    <table class="table table-bordered table-striped">
         <thead style="">
             <tr>
-
-                <th style="width: 20%" class="text-center">Nombre Tribunal</th>
-                <th style="width: 20%" class="text-center">Proyectos asignados</th>
-                <th style="width: 60%" class="text-center">Proyectos</th>
+                <th>Nombre Tribunal</th>
+                <th>Proyecto</th>
             </tr>
         </thead>
-        
-        <tbody id="datosTabla">
+        <tbody>
         @foreach($tribunales as $tribunal)
             <tr>
-                <td style="width: 20%">
-                    {{ $tribunal->apePaternoDoc }} {{ $tribunal->apeMaternoDoc }} {{ $tribunal->nombreDoc }}
-                </td>
-                <td style="width: 20%" class="text-center">{{ $tribunal->cantidad}}</td>
-                <td style="width: 60%">
-                    @foreach($tribunal->proyecto as $aux)
-                            {{$tribunal->i++}} {{ $aux}} <br>
-                    @endforeach
-                </td>
+                <td>{{ $tribunal->docente->nombreDoc }}</td>
+                <td>{{ $tribunal->proyecto->titulo }}</td>
             </tr>
         @endforeach
         </tbody>
     </table>
-    
+    {{ $tribunales->links() }}
 </div>
-<script>
-    
-
-    $(document).ready(function(){
-          $("#Search1").on("keyup", function() {
-            var value = $(this).val().toLowerCase();
-            $("#datosTabla tr").filter(function() {
-              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-            });
-          });
-        });
-</script>
 @endsection
-
