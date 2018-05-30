6ef6218924b85d565029089b9602529dc5f1d8e1
Luis Eduardo <ketchsoul@gmail.com>
2018-05-29 21:50:44 -0400

    solo actualizar we

--- resources/views/docentes/newDoc.blade.php
+++ resources/views/docentes/newDoc.blade.php
@@ -52,93 +52,130 @@
                <option value="Magister">Magister</option>-->
             </select>
         </div>
-        <!-- Grid column -->
 
-        <!-- Grid column -->
-        <div class="col-md-3">
-            <!-- Material input -->
-            <div class="md-form form-group">
-                <input type="text" class="form-control" id="ci">
-                <label >CI: </label>
-            </div>
-        </div>
+       <div class="col-md-4">
+           <label class="control-label">Carga Horaria: </label>
+                <select class="mdb-select colorful-select dropdown-primary" name="carga" id="carga" >
+               <option value=""></option>
+               <option value="Tiempo Parcial">Tiempo Parcial</option>
+               <option value="Tiempo Completo">Tiempo Completo</option>
+           </select>
 
-        <div class="col-md-3">
-            <!-- Material input -->
-            <div class="md-form form-group">
-                <input type="text" class="form-control" id="telefono">
-                <label >Telefono/Celular: </label>
-            </div>
-        </div>
-        <!-- Grid column -->
-    </div>
-       <label>Area: </label>
-        <div class="form-row">
-                <div class="col-md-12">
-                <select class="mdb-select colorful-select dropdown-primary" multiple name="area[]" id="area" required>
-                    <option active disabled>Seleccionar un Area</option>
-                    @foreach($areas as $area)
-                    <option value="{{ $area-> idArea}}"> {{ $area-> nombreArea}}</option>
-                    @endforeach
-                </select>
-                </div>
-        </div>  
+       </div>
+       <div class="col-md-4">
 
-    <!-- <button class="btn  btn-primary btn-md" id="add">GUARDAR</button>
-    <button class="btn  btn-info btn-md">CANCELAR</button> -->
-    <div class="group form-row" style=margin-top:60px;></div>
-    <div class="form-row">
-        <div class="col-md-8" ></div>
-        
-            <button type="reset" class="btn btn-red btn-rounded">
-                <font color="white" size="3">CANCELAR</font>
-			</button>
-            <!-- <button class="btn  btn-primary btn-md" id="add">GUARDAR</button> -->
-            <button class="btn btn-light-green btn-rounded" id="add">
-                <font color="white" size="3">GUARDAR</font>
-			</button>
-    </div>
+           <!-- Material input -->
+           <div class="md-form form-group">
+               <select class="mdb-select colorful-select dropdown-primary col-md-12" id="tituloDoc">
+                   <option value="Lic.">Licenciado(a)</option>
+                   <option value="Ing.">Ingeniero(a)</option>
+                   <option value="Msc.">Magister</option>
+               </select>
+               <label >*Titulo Docente: </label>
+           </div>
+
+           <!--<select class="mdb-select colorful-select dropdown-primary" multiple name="area" id="select">
+               <option disabled=""> Seleccione una o mas Areas</option>
+               @foreach($areas as $area)
+               <option value= {{ $area-> idArea}} > {{ $area-> nombreArea}} </option>
+               @endforeach
+           </select>-->
+
+       </div>
+   </div>
+     <div class="form-row">
+       <!-- Grid column -->
+       <div class="col-md-6">
+           <!-- Material input -->
+           <div class="md-form form-group">
+               <input type="email" class="form-control" id="email">
+               <label >Email</label>
+           </div>
+       </div>
+       <!-- Grid column -->
+
+       <!-- Grid column -->
+       <div class="col-md-3">
+           <!-- Material input -->
+           <div class="md-form form-group">
+               <input type="text" class="form-control" id="ci">
+               <label >CI: </label>
+           </div>
+       </div>
+
+       <div class="col-md-3">
+           <!-- Material input -->
+           <div class="md-form form-group">
+               <input type="text" class="form-control" id="telefono">
+               <label >Telefono/Celular: </label>
+           </div>
+       </div>
+       <!-- Grid column -->
+   </div>
+      <label>Area: </label>
+       <div class="form-row">
+               <div class="col-md-12">
+               <select class="mdb-select colorful-select dropdown-primary" multiple name="area[]" id="area" required>
+                   <option active disabled>Seleccionar un Area</option>
+                   @foreach($areas as $area)
+                   <option value="{{ $area-> idArea}}"> {{ $area-> nombreArea}}</option>
+                   @endforeach
+               </select>
+               </div>
+       </div> 
+
+   <!-- <button class="btn  btn-primary btn-md" id="add">GUARDAR</button>
+   <button class="btn  btn-info btn-md">CANCELAR</button> -->
+   <div class="group form-row" style=margin-top:60px;></div>
+   <div class="form-row">
+       <div class="col-md-8" ></div>
+      
+           <button type="reset" class="btn btn-red btn-rounded">
+               <font color="white" size="3">CANCELAR</font>
+            </button>
+           <!-- <button class="btn  btn-primary btn-md" id="add">GUARDAR</button> -->
+           <button class="btn btn-light-green btn-rounded" id="add">
+               <font color="white" size="3">GUARDAR</font>
+            </button>
+   </div>
 
 </form>
 @endsection
 @section('script')
 <script>
-    $(document).on('click', '#add', function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: '/docentes',
-            data: {
-                '_token': $('input[name=_token]').val(),
-                'ciDoc': $('#ci').val(),
-                'nombreDoc': $('#name').val(),
-                'apePaternoDoc': $('#apePaterno').val(),
-                'apeMaternoDoc': $('#apMaterno').val(),
-                'emailDoc': $('#email').val(),
-                'telefonoDoc': $('#telefono').val(),
-                'tituloDoc': $('#tituloDoc').val(),
-                'cargaHoraria': $('#carga').val(),
+   $(document).on('click', '#add', function(e) {
+       e.preventDefault();
+       $.ajax({
+           type: 'POST',
+           url: '/docentes',
+           data: {
+               '_token': $('input[name=_token]').val(),
+               'ciDoc': $('#ci').val(),
+               'nombreDoc': $('#name').val(),
+               'apePaternoDoc': $('#apePaterno').val(),
+               'apeMaternoDoc': $('#apMaterno').val(),
+               'emailDoc': $('#email').val(),
+               'telefonoDoc': $('#telefono').val(),
+               'tituloDoc': $('#tituloDoc').val(),
+               'cargaHoraria': $('#carga').val(),
 
-                'tipo': $('#tipo').val(),
-                'area': $('#area').val(),
+               'tipo': $('#tipo').val(),
+               'area': $('#area').val(),
 
-            },
-            success : function(data) {
-                toastr.success(data.message);
-                clear();
-                location.reload();
-
-            },
-            error : function(xhr, status) {
-                toastr.error('Disculpe, existio un problema!');
-            },
-        });
-    });
-    function clear() 
-    {
-        document.getElementById("miForm").reset();
-
-    }
+           },
+           success : function(data) {
+               toastr.success(data.message);
+               clear();
+           },
+           error : function(xhr, status) {
+               toastr.error('Disculpe, existio un problema!');
+           },
+       });
+   });
+   function clear()
+   {
+       document.getElementById("miForm").reset();
+   }
 </script>
 
 @endsection
